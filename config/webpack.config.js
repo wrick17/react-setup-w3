b40373b51cb27b098ea1b6ef9fb3027ae07e05d1
Pratyush Poddar <wrick17@gmail.com>
2017-07-11 20:03:27 +0530

    added eslint in webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path")
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: {
     app: [
-      'react-hot-loader/patch',
-      './src/index.js'
-    ]
+      "react-hot-loader/patch",
+      "./src/index.js",
+    ],
   },
   resolve: {
     alias: {
-      images: path.resolve('./assets/images'),
-      fonts: path.resolve('./assets/fonts'),
-      constants: path.resolve('./assets/constants/constants.js'),
-      containers: path.resolve('./src/containers'),
-      components: path.resolve('./src/components')
-    }
+      images: path.resolve("./assets/images"),
+      fonts: path.resolve("./assets/fonts"),
+      constants: path.resolve("./assets/constants/constants.js"),
+      containers: path.resolve("./src/containers"),
+      components: path.resolve("./src/components"),
+    },
   },
   output: {
-    filename: '[name].[hash].js',
-    path: path.resolve('./dist/'),
+    filename: "[name].[hash].js",
+    path: path.resolve("./dist/"),
     publicPath: "/",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Potato',
-      template: './src/index.ejs',
-      appMountId: 'root',
-      favicon: './assets/images/favicon.png'
+      title: "Potato",
+      template: "./src/index.ejs",
+      appMountId: "root",
+      favicon: "./assets/images/favicon.png",
     }),
     new webpack.optimize.ModuleConcatenationPlugin(),
     new webpack.HotModuleReplacementPlugin(), // Enable HMR
@@ -36,26 +36,32 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: "pre",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader",
+      },
+      {
         test: /\.js$/,
         use: [
-          'babel-loader'
+          "babel-loader",
         ],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader?name=images/[name].[ext]'
-        ]
+          "file-loader?name=images/[name].[ext]",
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         exclude: /node_modules/,
         use: [
-          'file-loader?name=assets/fonts/[name].[ext]'
-        ]
-      }
-    ]
+          "file-loader?name=assets/fonts/[name].[ext]",
+        ],
+      },
+    ],
   },
   devtool: "cheap-module-source-map",
   devServer: {
@@ -64,6 +70,6 @@ module.exports = {
     publicPath: "/",
     compress: true,
     historyApiFallback: true,
-    port: 9000
-  }
-};
+    port: 9000,
+  },
+}
