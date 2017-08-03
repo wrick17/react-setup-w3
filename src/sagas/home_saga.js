8b4c13edb95b79a29ceb7b660592ca46add70354
Pratyush Poddar <wrick17@gmail.com>
2017-08-03 12:14:22 +0530

    100 percent hot

--- src/sagas/home_saga.js
+++ src/sagas/home_saga.js
@@ -3,7 +3,6 @@ import { put } from 'redux-saga/effects';
 import ACTION from 'common/action_constants.js';
 function* home(action) {
     yield delay(1000);
-    console.log('hello bello');
     yield put({type:ACTION.HOME.LOADHOME});
 }
 export {
